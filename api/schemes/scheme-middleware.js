5f4bf7e8e40aa677c2952ca8399e63189788cd9c
Elizabeth Doyle <123044077+elizabethmdoyle@users.noreply.github.com>
2024-02-26 13:45:14 -0500

    2 passing tests

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -1,3 +1,6 @@
+const Scheme = require('./scheme-model');
+
+
 /*
   If `scheme_id` does not exist in the database:
 
@@ -6,7 +9,20 @@
     "message": "scheme with scheme_id <actual id> not found"
   }
 */
-const checkSchemeId = (req, res, next) => {
+const checkSchemeId = async (req, res, next) => {
+
+  try {
+    const scheme = await Scheme.findById(req.params.id);
+
+    if(!scheme) {
+      next({status:404,message: `scheme with scheme_id ${req.params.id} not found` })
+    } else {
+      req.car = car
+      next()
+    }
+  } catch(err) {
+    next(err)
+  }
 
 }
 
@@ -18,7 +34,14 @@ const checkSchemeId = (req, res, next) => {
     "message": "invalid scheme_name"
   }
 */
-const validateScheme = (req, res, next) => {
+const validateScheme = async (req, res, next) => {
+
+  const {scheme_name} = req.body;
+
+  if ( !scheme_name || typeof(scheme_name) !== 'string' || !scheme_name.trim() ) {
+    next({status: 400, message: 'invalid scheme_name'})
+  }
+
 
 }
 
@@ -32,6 +55,13 @@ const validateScheme = (req, res, next) => {
   }
 */
 const validateStep = (req, res, next) => {
+
+  const {instructions, step_number } = req.body;
+
+  if ( !instructions || typeof(instructions) !== 'string' || !instructions.trim() || isNaN(step_number) || step_number < 1 ) {
+    next({status: 400, message: 'invalid step'})
+  }
+
 
 }
 
--- api/server.js
+++ api/server.js
@@ -7,4 +7,14 @@ const server = express();
 server.use(express.json());
 server.use('/api/schemes', SchemeRouter);
 
+server.use('*', (req, res) => {
+    res.status(404).json({ message: 'endpoint not found'})
+})
+
+server.use((err, req, res, next) => { //eslint-disable-line
+    res.status(err.status || 500).json({
+        message: err.message,
+    })
+})
+
 module.exports = server;
